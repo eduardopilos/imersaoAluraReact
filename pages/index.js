15e892dbb3eca74c1f6533568afa2e997ae92225
Eduardo Georges de Oliveira Pilos <eduardo.pilos@hotmail.com>
2021-08-02 16:27:32 -0300

    Definição de componentes em pastas

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src="https://github.com/eduardopilos.png"/>
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Amigos
+        </Box>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
---
+++ src/components/Box/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+  margin-bottom: 10px;
+
+  .boxLink{
+      font-size: 14px;
+      color: #2E78884;
+      text-decoration: none;
+      font-weight: 800;
+  }
+`;
+export default Box;
