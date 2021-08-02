85eec59117c3c407864af5d7d7005307c17f72e6
Eduardo Georges de Oliveira Pilos <eduardo.pilos@hotmail.com>
2021-08-02 17:51:16 -0300

    Final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,59 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
+import {AlurakutMenu} from '../src/lib/AlurakutCommons'
+
+
+function ProfileSidebar(propriedades){
+  
+  return(
+    <Box>
+      <img style ={{ borderRadius: '8px'}} src={`https://github.com/${propriedades.gitHubUser}.png`}/>
+    </Box>
+  )
+}
+
 
 export default function Home() {
+  const gitHubUser = 'eduardopilos';
+  const amigos = [
+    'Edemundo',
+    'LucasVmigotto',
+    'peas',
+    'pedssodre',
+    'gdiasbruno',
+    'juunegreiros'
+  ]
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <Box>
-          <img src="https://github.com/eduardopilos.png"/>
-        </Box>
-      </div>
-      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-        <Box>
-          Bem vindo
-        </Box>
-      </div>
-      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-        <Box>
-          Amigos
-        </Box>
-        <Box>
-          Comunidades
-        </Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu/>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar gitHubUser={gitHubUser}/>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            Bem vindo
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Amigos ({amigos.length})</h2>
+            <ul>
+              {amigos.map((itemAtual) => {
+                return (
+                  <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`}/>
+                    <span>{itemAtual}</span>
+                  </a>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
   )
 }
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -3,12 +3,55 @@ const Box = styled.div`
   background: #ffffff;
   border-radius: 8px;
   margin-bottom: 10px;
+  padding: 16px;
 
   .boxLink{
       font-size: 14px;
       color: #2E78884;
       text-decoration: none;
       font-weight: 800;
+  }
+  .title{
+      font-size: 32px;
+      fonte-weight: 400;
+      margin-bottom: 20px;
+  }
+  .subTitle{
+      font-size: 10px;
+      font-weight: 400;
+      margin-bottom: 20px;
+  }
+  .smallTitle{
+      margin-bottom: 20px;
+      font-size: 16px;
+      font-weight: 200;
+      color: #333333;
+      margin-bottom: 20px;
+  }
+  hr{
+      margin-top: 12px;
+      margin-bottom: 8px;
+      border-color: transparent;
+      border-bottom-color: #ECF2FA;
+  }
+  input{
+      width: 100%;
+      background-color: #F4F4F4;
+      color: #333333;
+      border: 0;
+      padding: 14px 16px;
+      border-radius: 10000px;
+      ::placeholder{
+          color: #333333;
+          opacity: 1;
+      }
+  }
+  button {
+      border: 0;
+      padding: 8px 12px;
+      color: #FFFFFF;
+      border-radius: 10000px;
+      background-color: #6F92BB;
   }
 `;
 export default Box;
